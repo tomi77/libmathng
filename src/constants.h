4a59d6af870f4832f18472f6efadc284b274f734
Tomasz Jakub Rup <tomasz.rup@gmail.com>
2011-09-16 20:06:28 +0200

    clean

---
+++ src/constants.h
@@ -0,0 +1,16 @@
+#ifndef _MATH_CONSTANTS_H_
+#define _MATH_CONSTANTS_H_ 1
+
+namespace math { namespace constants {
+
+static const double pi = 3.141592653589793238462643383279502884197169399375105820974944;
+
+/**
+ * Euler's number
+ */
+static const double e = 2.7182818284590452353602874713526624977572470936999595749669676;
+
+}} /* namespace math::constants */
+
+#endif /* _MATH_CONSTANTS_H_ */
+
